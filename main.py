09921ac41b2e18e4a9e0812bd68c9f87a96f365d
RishiRats <64166728+RishiRats@users.noreply.github.com>
2023-02-25 15:22:38 +0530

    addded Date feature

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 # importing libraries
 from imap_tools import MailBox, AND
+import datetime
+from pytz import timezone
 
 # taking user's credentials
+username = input('Username: ')
+password = input('password: ')
 
-#username = input('Username: ')
-#password = input('\nPassword: ')
-username = 'testuser_elpida@outlook.com'
-password = '_elpidatestuser@testing'
-
+# Setting variables
 seen = int(0)
 flags = int(0)
+
+# Setting Period
+print('***************************************')
+DD = int(input('Input start DD: '))
+MM = int(input('Input start MM: '))
+YYYY = int(input('Input start YYYY: '))
+startdate = datetime.datetime(YYYY, MM, DD, 0, 0, 0)
+
+nDD = int(input('\nInput end DD: '))
+nMM = int(input('Input end MM: '))
+nYYYY = int(input('Input end YYYY: '))
+enddate = datetime.datetime(nYYYY, nMM, nDD, 0, 0, 0)
+print('***************************************')
 
 # mainstuff
 with MailBox('outlook.office365.com').login( username, password) as mailbox:
     for msg in mailbox.fetch():
-        if (msg.flags[0]) == ('\Seen'):
-            seen = seen + 1
+        if startdate<(msg.date).replace(tzinfo=None)<enddate:
+            if (msg.flags[0]) == ('\Seen'):
+                seen = seen + 1
+            else:
+                flags = flags + 1
         else:
-            flags = flags + 1
+            print('Please choose correct period')
 print('seen: {}, flags: {}'.format(seen, flags))
