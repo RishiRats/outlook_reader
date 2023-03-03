d1d0d13dce81de1195eda2fc48c5c93743e1d777
Jkdxbns <jmascarenhas1998@gmail.com>
2023-03-03 23:30:29 +0530

    justin edit

--- trial.py
+++ trial.py
@@ -24,21 +24,6 @@ box_text = boxing("  MADE BY AVANA\nEmail Book-Keeper", style='double')
 print(box_text, file=sys.stdout)
 
 
-def ask_for_date(var):
-    """Function asking for date"""
-    while True:
-        try:
-            DD = int(input(f'Enter {var} D: '))
-            MM = int(input(f'Enter {var} M: '))
-            YYYY = int(input(f'Enter {var} YYYY: '))
-            date = datetime.datetime(YYYY, MM, DD)
-
-            return date
-        except ValueError:
-            print('Enter date in correct format')
-            pass
-
-
 # taking user's credentials
 while True:
     username = input('Enter email: ')
@@ -54,32 +39,24 @@ while True:
     except:
         print('Enter valid credentials')
 
-# Ask for start and end date
-print('Please Input Dates in Ascending Order.')
-start_date = timezone.localize(ask_for_date('Start'))
-end_date = timezone.localize(ask_for_date('End'))
-
 # for instant use only date and remove _gte
 # Logins to given credentials and read
 print('Fetching inbox details...')
 with MailBox(MAIL_SERVER).login(username, password) as mailbox:
     # Bye default inbox is fetched and in reversed order
-    for msg in mailbox.fetch(A(date_gte=start_date.date()), mark_seen=False, reverse=True):
-        if start_date < msg.date < end_date:
-            sender = msg.from_
-            if sender in mails_dict.keys():
-                # increments email count from one sender
-                mails_dict[sender]['Total'] += 1
-            else:
-                mails_dict[sender] = {'Total': 1, 'Seen': 0, 'Flagged': 0, 'Answered': 0}
-            total_mail_count = total_mail_count + 1
+    for msg in mailbox.fetch(A(date=datetime.date(2023, 3, 2)), mark_seen=False, reverse=True):
+        sender = msg.from_
+        if sender in mails_dict.keys():
+            # increments email count from one sender
+            mails_dict[sender]['Total'] += 1
+        else:
+            mails_dict[sender] = {'Total': 1, 'Seen': 0, 'Flagged': 0, 'Answered': 0}
+        total_mail_count = total_mail_count + 1
 
-            mails_dict[sender]['Seen'] = msg.flags.count('\\Seen')
-            mails_dict[sender]['Answered'] = msg.flags.count('\\Answered')
-            mails_dict[sender]['Flagged'] = msg.flags.count('\\Flagged')
+        mails_dict[sender]['Seen'] = msg.flags.count('\\Seen')
+        mails_dict[sender]['Answered'] = msg.flags.count('\\Answered')
+        mails_dict[sender]['Flagged'] = msg.flags.count('\\Flagged')
 
-        elif msg.date < start_date:
-            break
 
 # Opens excel workbook
 wb = Workbook()
